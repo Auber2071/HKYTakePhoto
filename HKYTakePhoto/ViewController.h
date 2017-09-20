885033030844642d2b083a43f0fdd2b4731b3ca7
ALPS.K <hankai@MacBook-hankai.local>
2017-09-20 10:25:15 +0800

    Initial Commit

---
+++ HKYTakePhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HKYTakePhoto
+//
+//  Created by hankai on 2017/9/20.
+//  Copyright © 2017年 Vencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
